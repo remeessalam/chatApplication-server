46975986f8bfee867f5509c65c43556d30393458
Remees Salam <remeessalam@Remeess-Air.bbrouter>
2023-02-15 13:43:34 +0530

    finish this appliction want to impliment more function to this app

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
-import {config} from "dotenv";
+import { config } from "dotenv";
 config();
 import fastify from "fastify";
-
+import cors from "@fastify/cors"
+import { userRoutes } from './routes/users'
 const app = fastify();
-
+app.register(cors, { origin: process.env.CLIENT_URL })
+app.register(userRoutes)
 console.log("server port is running")
 
-app.listen({port : parseInt(process.env.PORT!)});
+app.listen({ port: parseInt(process.env.PORT!) });
